01909fb3e6fa2314ac448111ab7dcba9220c97c8
Maurits Lawende <mauritsl@users.noreply.github.com>
2018-08-20 15:01:32 +0200

    Fix update script

--- config/scripts/securityupdates.sh
+++ config/scripts/securityupdates.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-node /var/scripts/securityupdates.js
-
 cd /var/www
-drush make-update-exclude --include=/var/scripts/drush -y --update-backend=drush --result-file=drush.make.yml --security-only drush.make.yml
 
-echo "Changed: /var/www/drush.make.yml => /drush.make.yml"
+if [ -f "drush.make.yml" ]; then
+  drush make-update-exclude --include=/var/scripts/drush -y --update-backend=drush --result-file=drush.make.yml --security-only drush.make.yml
+  echo "Changed: /var/www/drush.make.yml => /drush.make.yml"
+fi
+
+if [ -f "drush.make" ]; then
+  drush make-update-exclude --include=/var/scripts/drush -y --update-backend=drush --result-file=drush.make --format=ini --security-only drush.make
+  echo "Changed: /var/www/drush.make => /drush.make"
+fi
+
+if [ -f "src/drush.make" ]; then
+  drush make-update-exclude --include=/var/scripts/drush -y --update-backend=drush --result-file=src/drush.make --format=ini --security-only src/drush.make
+  echo "Changed: /var/www/src/drush.make => /src/drush.make"
+fi
+
+if [ -f "src/project.make" ]; then
+  drush make-update-exclude --include=/var/scripts/drush -y --update-backend=drush --result-file=src/project.make --format=ini --security-only src/project.make
+  echo "Changed: /var/www/src/project.make => /src/project.make"
+fi
