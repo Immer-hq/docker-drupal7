a1191a51f07b4a435fd915ea400a907848a144f8
Maurits Lawende <mauritsl@users.noreply.github.com>
2019-01-11 11:39:03 +0100

    Add src/drush.make.yml as makefile location

--- config/scripts/securityupdates.sh
+++ config/scripts/securityupdates.sh
@@ -17,6 +17,11 @@ if [ -f "src/drush.make" ]; then
   echo "Changed: /var/www/src/drush.make => /src/drush.make"
 fi
 
+if [ -f "src/drush.make.yml" ]; then
+  drush make-update-exclude --include=/var/scripts/drush -y --update-backend=drush --result-file=src/drush.make.yml --format=ini --security-only src/drush.make.yml
+  echo "Changed: /var/www/src/drush.make.yml => /src/drush.make.yml"
+fi
+
 if [ -f "src/project.make" ]; then
   drush make-update-exclude --include=/var/scripts/drush -y --update-backend=drush --result-file=src/project.make --format=ini --security-only src/project.make
   echo "Changed: /var/www/src/project.make => /src/project.make"
